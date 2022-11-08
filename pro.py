f7e54d86d5e3ad1b9fd7066d41d7dc3a97b4dd65
Alz Kyra? <muzakkyoffice20@gmail.com>
2022-11-08 07:54:16 +0700

    Salman

---
+++ pro.py
@@ -0,0 +1,20 @@
+Muzakky = "Hello World to Python Salman"
+print (Muzakky)
+
+
+print (1+1)
+print (100-20)
+print (20*90)
+print (20//2)
+
+var1 = 10
+var2 = 20
+var3 = (var1 / var2)
+var4 = (var1 // var2) 
+print (var3)
+print (var4)
+
+Sal1 = "Usiaku"
+Sal2 = "17"
+
+print (Sal1, Sal2)
